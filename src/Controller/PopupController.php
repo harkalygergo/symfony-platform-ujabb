6f192a896a6652d1100c767f03f87ceece274ca0
Harkály Gergő <git@harkalygergo.hu>
2024-07-01 13:32:56 +0200

    test

---
+++ src/Controller/PopupController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Harkalygergo\SymfonyPlatform\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class PopupController extends AbstractController
+{
+    public function __construct()
+    {
+        return new Response('Hello Gergő!');
+    }
+}
