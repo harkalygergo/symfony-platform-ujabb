c360f9431b097d17b714e18336339676ccac5840
Harkály Gergő <git@harkalygergo.hu>
2024-07-01 13:52:14 +0200

    test2

--- src/Controller/PopupController.php
+++ src/Controller/PopupController.php
@@ -9,6 +9,10 @@ class PopupController extends AbstractController
 {
     public function __construct()
     {
+    }
+
+    public function fancy(): Response
+    {
         return new Response('Hello Gergő!');
     }
 }
