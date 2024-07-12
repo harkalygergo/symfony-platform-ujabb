092a1f1bd4cb09911853e0b73ae80782d5dc2d28
Harkály Gergő <git@harkalygergo.hu>
2024-07-12 14:14:19 +0200

    2024.07.12.8 - return start with request and locale

--- src/Controller/PopupController.php
+++ src/Controller/PopupController.php
@@ -3,12 +3,18 @@
 namespace HarkalyGergo\SymfonyPlatform\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class PopupController extends AbstractController
 {
-    public function start(): Response
+    public function start(Request $request): JsonResponse
     {
-        return new Response('Hello Gergő!');
+        // return with $request and locale
+        return new JsonResponse([
+            'request' => $request->query->all(),
+            'locale' => $request->getLocale(),
+        ]);
     }
 }
